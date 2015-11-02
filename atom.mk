0421b6274729f68b284fcc2c48a7cbab9bc4b14b
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-02 07:49:01 +0100

    added base atom.mk

---
+++ atom.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := adbd
+LOCAL_CATEGORY_PATH := system
+
+LOCAL_AUTOTOOLS_MAKE_BUILD_ARGS := \
+	VPATH=$(LOCAL_PATH)/..
+
+LOCAL_AUTOTOOLS_MAKE_INSTALL_ARGS := \
+	$(LOCAL_AUTOTOOLS_MAKE_BUILD_ARGS) \
+	prefix=$(TARGET_OUT_STAGING)/usr
+
+ADBD_BUILD_DIR := $(call local-get-build-dir)
+define LOCAL_AUTOTOOLS_CMD_CONFIGURE
+	$(Q) cp -pf $(PRIVATE_PATH)/Makefile $(ADBD_BUILD_DIR)/obj/
+endef
+
+include $(BUILD_AUTOTOOLS)
+
